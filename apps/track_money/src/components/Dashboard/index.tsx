48e9babe92868adc525af2049bc580bfb65de5ad
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-10-25 07:35:27 -0300

    feat: add Dashboard component

---
+++ apps/track_money/src/components/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import { DashboardStyle } from "./styles"
+
+export function Dashboard () {
+  return (
+    <DashboardStyle>
+      Dashboard
+    </DashboardStyle>
+  )
+}
---
+++ apps/track_money/src/components/Dashboard/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const DashboardStyle = styled.main`
+  margin: 0 auto;
+  max-width: 1120;
+  padding: 2.5rem 1rem;
+`
