52678ec3c60fd911ed84777e07931f25b5436f6e
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-10-29 09:13:11 -0300

    chore: add config to fake server

--- apps/track_money/src/index.tsx
+++ apps/track_money/src/index.tsx
@@ -1,6 +1,50 @@
 import { render } from 'react-dom'
+import { createServer } from 'miragejs'
 
 import{ App } from './App'
+
+createServer({
+  routes(){
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de site',
+          amount: 10000,
+          type: 'deposit',
+          category: 'freelancer',
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Despesas gerais',
+          amount: 500,
+          type: 'withdraw',
+          category: 'casa',
+          createdAt: new Date()
+        },
+        {
+          id: 4,
+          title: 'Trabalho',
+          amount: 6000,
+          type: 'deposit',
+          category: 'trabalho',
+          createdAt: new Date()
+        },
+        {
+          id: 8,
+          title: 'Parcela do carro',
+          amount: 500,
+          type: 'withdraw',
+          category: 'casa',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 
 render(<App/>, document.getElementById('root'))
