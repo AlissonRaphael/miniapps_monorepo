b8d4068b149b58edefb9c9b2f1cfb8ae07292e73
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-11-04 08:15:31 -0300

    refactor: change svg filters

--- apps/track_money/src/components/Summary/index.tsx
+++ apps/track_money/src/components/Summary/index.tsx
@@ -1,24 +1,7 @@
 import { SummaryStyle } from "./styles"
-import ArrowCircle from '../../assets/arrow_circle_black_24dp.svg'
 import Money from '../../assets/monetization_on_black_24dp.svg'
-
-
-const upArrow = {
-  height: '30px',
-  filter: `invert(64%) sepia(93%) saturate(306%) hue-rotate(106deg) brightness(87%) contrast(95%)`,
-  transform: 'rotate(-90deg)'
-}
-
-const downArrow = {
-  height: '30px',
-  filter: `invert(50%) sepia(34%) saturate(5298%) hue-rotate(332deg) brightness(114%) contrast(101%)`,
-  transform: 'rotate(90deg)'
-}
-
-const money = {
-  height: '30px',
-  filter: `invert(100%)`,
-}
+import ArrowCircle from '../../assets/arrow_circle_black_24dp.svg'
+import { svgFilter } from "../../styles/svg-filters"
 
 export function Summary(){
   return (
@@ -26,7 +9,7 @@ export function Summary(){
       <div>
         <header>
           <p>Entradas</p>
-          <img src={ArrowCircle} style={upArrow} alt="Entradas de dinheiro"/>
+          <img src={ArrowCircle} style={svgFilter.upArrow} alt="Entradas de dinheiro"/>
         </header>
         <strong>R$ 1000,00</strong>
       </div>
@@ -34,7 +17,7 @@ export function Summary(){
       <div>
         <header>
           <p>Saidas</p>
-          <img src={ArrowCircle} style={downArrow} alt="Saidas de dinheiro"/>
+          <img src={ArrowCircle} style={svgFilter.downArrow} alt="Saidas de dinheiro"/>
         </header>
         <strong>R$ -600,00</strong>
       </div>
@@ -42,7 +25,7 @@ export function Summary(){
       <div>
         <header>
           <p>Total</p>
-          <img src={Money} style={money} alt="Dinheiro total"/>
+          <img src={Money} style={svgFilter.money} alt="Dinheiro total"/>
         </header>
         <strong>R$ 400,00</strong>
       </div>
---
+++ apps/track_money/src/styles/svg-filters.ts
@@ -0,0 +1,16 @@
+export const svgFilter = {
+  upArrow: {
+    height: '30px',
+    filter: `invert(64%) sepia(93%) saturate(306%) hue-rotate(106deg) brightness(87%) contrast(95%)`,
+    transform: 'rotate(-90deg)'
+  },
+  downArrow: {
+    height: '30px',
+    filter: `invert(50%) sepia(34%) saturate(5298%) hue-rotate(332deg) brightness(114%) contrast(101%)`,
+    transform: 'rotate(90deg)'
+  },
+  money: {
+    height: '30px',
+    filter: `invert(100%)`,
+  }
+}
