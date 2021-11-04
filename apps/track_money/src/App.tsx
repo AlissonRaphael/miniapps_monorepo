ca7220031b07804fa3b4e1af5f9f63f2805252a6
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-11-04 08:02:33 -0300

    refactor: update global styles

--- apps/track_money/src/App.tsx
+++ apps/track_money/src/App.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import Modal from 'react-modal'
 
 import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header/'
-import { Modal as ModalContent } from './components/Modal'
+import { Modal } from './components/Modal'
 
 import { GlobalStyle } from './styles/global'
 
-Modal.setAppElement('#root')
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
@@ -24,12 +22,7 @@ export function App() {
     <>
       <GlobalStyle/>
       <Header handleOpenNewTransactionModal={handleOpenNewTransactionModal}/>
-      <Modal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-        <ModalContent/>
-      </Modal>
+      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <Dashboard/>
     </>
   )
