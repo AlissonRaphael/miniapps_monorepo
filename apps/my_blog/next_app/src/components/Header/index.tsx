65825cf0612e3f762930d3092367b0975410fe1e
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-11-30 07:20:13 -0300

    feat: create Header component

---
+++ apps/my_blog/next_app/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import styles from './styles.module.scss'
+
+export function Header(){
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <div className={styles.logo}>
+          <img className={styles.logo} src="./images/logo.png" alt="My Blog" />
+          <span>my_blog</span>
+        </div>
+        <nav>
+          <a className={styles.active} href="#">Home</a>
+          <a href="#">Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
