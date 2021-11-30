93478d54b5e78d8f535dd949c56668c733808e8a
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-11-30 07:19:41 -0300

    rafactor: remove test line

--- apps/my_blog/next_app/src/pages/index.tsx
+++ apps/my_blog/next_app/src/pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 
+
 export default function Home() {
   return (
     <>
       <Head><title>Home | My Blog</title></Head>
-      <h1>Hello <span>Alisson</span>!</h1>
     </>
   )
 }
