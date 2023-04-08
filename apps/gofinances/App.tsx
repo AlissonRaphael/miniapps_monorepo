331ed03a4f0ba82bfbbdbd60fb69a694884cb268
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-04-08 20:18:51 -0300

    feat: implements theme colors

--- apps/gofinances/App.tsx
+++ apps/gofinances/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import Dashboard from './src/screens/Dashboard';
 
+import Theme from './src/global/theme'
+
 export default function App() {
   return (
-    <Dashboard />
+    <ThemeProvider theme={Theme}>
+      <Dashboard />
+    </ThemeProvider>
   )
 }
