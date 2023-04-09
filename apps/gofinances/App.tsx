c22cbc85c282180aad3fc658e73bccf614a7193a
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-04-08 21:25:51 -0300

    feat: implements font loading

--- apps/gofinances/App.tsx
+++ apps/gofinances/App.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { View, Text } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import * as SplashScreen from 'expo-splash-screen';
 
 import Dashboard from './src/screens/Dashboard';
 
-import Theme from './src/global/theme'
+import Theme from './src/global/theme';
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular, Poppins_500Medium, Poppins_700Bold
+  })
+
+  const onLayoutView = useCallback(async () => {
+    if (!fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return <View><Text>Loading</Text></View>
+  }
+
   return (
-    <ThemeProvider theme={Theme}>
-      <Dashboard />
-    </ThemeProvider>
+    <View style={{ flex: 1 }} onLayout={onLayoutView}>
+      <ThemeProvider theme={Theme}>
+        <Dashboard />
+      </ThemeProvider>
+    </View>
   )
 }
