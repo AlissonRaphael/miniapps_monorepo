f423ed85acc68b8c26754e35545d8f9e9230d685
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-04-15 10:53:33 -0300

    feat: typing transaction component

--- apps/gofinances/src/components/Transaction/index.tsx
+++ apps/gofinances/src/components/Transaction/index.tsx
@@ -1,22 +1,38 @@
 import { Container, Title, Amount, Footer, Description, Icon, Category, TransactionDate } from "./style"
 
-export default function Transaction () {
+interface Category {
+  name: string,
+  icon: string,
+}
+
+interface TransactionProps {
+  data: {
+    title: string,
+    amount: number,
+    category: Category,
+    date: Date,
+  }
+}
+
+export default function Transaction ({ data }: TransactionProps) {
+  const { title, amount, category, date } = data
+
   const formmatedDate = new Intl.DateTimeFormat('pt-BR',
     { year: "numeric", month: "long", day: "numeric", }
-  ).format(new Date(Date.now()))
+  ).format(date)
 
   const formmatedAmount = new Intl.NumberFormat('pt-BR',
     { style: 'currency', currency: 'BRL' }
-  ).format(11382)
+  ).format(amount)
 
   return (
     <Container>
-      <Title>Desenvolvimento</Title>
+      <Title>{title}</Title>
       <Amount type="deposit">{formmatedAmount}</Amount>
       <Footer>
         <Description>
-          <Icon name="dollar"/>
-          <Category>Trabalho</Category>
+          <Icon name={category.icon}/>
+          <Category>{category.name}</Category>
         </Description>
         <TransactionDate>{formmatedDate}</TransactionDate>
       </Footer>
