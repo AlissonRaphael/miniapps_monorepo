9bdd10029e9dd52d7c964d8ae5f6758bc6e1de12
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-04-19 21:11:05 -0300

    feat: creates submit-button

---
+++ apps/gofinances/src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import { TouchableOpacityProps } from 'react-native/types'
+
+import { Button, Label } from './styles'
+
+interface ButtonFormProps extends TouchableOpacityProps {
+  label: string
+}
+
+export default function ButtonForm ({ label, ...props }: ButtonFormProps) {
+  return (
+    <Button {...props}>
+      <Label>{label}</Label>
+    </Button>
+  )
+}
---
+++ apps/gofinances/src/components/Button/styles.ts
@@ -0,0 +1,22 @@
+import { TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Button = styled(TouchableOpacity).attrs((props) => ({
+  ...props,
+  activeOpacity: props.activeOpacity || 0.6,
+}))`
+  border-radius: 5px;
+  width: 100%;
+  padding: 18px;
+
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${({ theme }) => theme.colors.secondary};
+`
+
+export const Label = styled.Text`
+  font-size: 14px;
+  font-family: ${({ theme }) => theme.fonts.medium};
+  color: ${({ theme }) => theme.colors.shape};
+`
