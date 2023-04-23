883212fc0d23a3b28f6d7f4f8e6646fba22cf748
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-04-23 12:17:10 -0300

    feat: adds select-input in Register

--- apps/gofinances/src/screens/Register/index.tsx
+++ apps/gofinances/src/screens/Register/index.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
+
 import { Container, Header, Title, Form, Fields, Types } from "./styles";
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import Type from "../../components/Type";
-import { useState } from "react";
+import Select from "../../components/Select";
 
 export default function Register () {
   const [transactionType, setTransactionType] = useState<string>("")
@@ -37,6 +39,7 @@ export default function Register () {
             />
           </Types>
 
+          <Select title="Category" />
         </Fields>
 
         <Button label="Cadastrar" />
