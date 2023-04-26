3e9a194c373ba5003957c59e7e059f9c72fd0dca
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-04-26 08:24:42 -0300

    feat: adds error feedback in text-input

--- apps/gofinances/src/components/Input/index.tsx
+++ apps/gofinances/src/components/Input/index.tsx
@@ -1,24 +1,27 @@
 import { Control, Controller } from 'react-hook-form';
 import { TextInputProps } from 'react-native/types';
 
-import { Input } from './styles';
+import { Input, Error } from './styles';
 
 export interface InputProps extends TextInputProps {
   name: string,
+  error: any
 }
 
 interface InputFormProps extends InputProps {
-  control: Control,
+  control: Control
 }
 
-export default function InputForm ({ name, control, ...props }: InputFormProps) {
+export default function InputForm ({ name, control, error, ...props }: InputFormProps) {
   return (
     <Controller 
       name={name}
       control={control}
-      render={({ field: { onChange, value } }) => (
-        <Input name={name} onChangeText={onChange} value={value} {...props} />
-      )}
+      render={({ field: { onChange, value } }) => <>
+        <Input name={name} onChangeText={onChange} value={value} error={error} {...props} />
+        {error ? <Error>{error}</Error> : null }
+      </>
+      }
     />
   )
 }
--- apps/gofinances/src/components/Input/styles.ts
+++ apps/gofinances/src/components/Input/styles.ts
@@ -1,10 +1,10 @@
 import { TextInput } from 'react-native';
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 import { InputProps } from '.'
 
 export const Input = styled(TextInput)<InputProps>`
-  margin-bottom: 8px;
+  margin-bottom: ${({ error }) => error ? 2 : 8}px;
   border-radius: 5px;
   width: 100%;
   padding: 16px 18px;
@@ -13,4 +13,15 @@ export const Input = styled(TextInput)<InputProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   color: ${({ theme }) => theme.colors.text_dark};
   background-color: ${({ theme }) => theme.colors.shape};
+
+  ${({ error }) => error && css`
+    border: 1px solid ${({ theme }) => theme.colors.attention}
+  `};
+`
+
+export const Error = styled.Text`
+  font-size: 12px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.attention};
+  margin-bottom: 8px;
 `
