ef62092da27d5941b2e7c5f34fccd69ddf8be4fb
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-05-06 18:34:05 -0300

    feat: implements Summary

--- apps/gofinances/src/screens/Summary/index.tsx
+++ apps/gofinances/src/screens/Summary/index.tsx
@@ -1,9 +1,46 @@
-import { View, Text } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { Container, Header, Title } from './styles';
+
+import { $transactions } from '../../global/storage';
+import { TransactionItemProps } from '../../components/Transaction';
+import ActivityIndicator from '../../components/ActivityIndicator';
+import AmountList from '../../components/AmountList';
+
+export interface TransactionListProps extends TransactionItemProps {
+  id: string,
+}
 
 export default function Summary () {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [transactions, setTransactions] = useState<TransactionListProps[]>([])
+
+  const loadTransactions = async () => {
+    setIsLoading(true)
+    const data = await AsyncStorage.getItem($transactions)
+    if (data) {
+      setTransactions(JSON.parse(data || "[]"))
+    }
+    setIsLoading(false)
+  }
+
+  useFocusEffect(useCallback(() => {
+    loadTransactions()
+  }, []))
+
+  if (isLoading) {
+    return <ActivityIndicator />
+  }
+
   return (
-    <View>
-      <Text>Summary - TODO</Text>
-    </View>
+    <Container>
+      <Header>
+        <Title>Resumo</Title>
+      </Header>
+
+      <AmountList transactions={transactions} />
+    </Container>
   )
 }
---
+++ apps/gofinances/src/screens/Summary/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+import { StatusBarHeightHelper, BottomHeightHelper } from '../../global/statusbar';
+
+export const Container = styled.View`
+  padding-bottom: ${BottomHeightHelper()}px;
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background };
+`
+
+export const Header = styled.View`
+  padding-top: ${StatusBarHeightHelper()}px;
+
+  width: 100%;
+  height: 114px;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+`
+
+export const Title = styled.Text`
+  font-size: 18px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.shape};
+`
