8657fd5d4309c3eb41550fe43cddb1fd34e586e6
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-05-07 20:58:19 -0300

    refactor: improvements in Amount comp

--- apps/gofinances/src/components/AmountList/index.tsx
+++ apps/gofinances/src/components/AmountList/index.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useMemo } from 'react';
 
-import { Amount, Container, Item, Title } from './styles';
+import { Container, Item, Title, Amounts, Icon, Amount } from './styles';
 
 import { TransactionItemProps } from '../Transaction';
 
@@ -11,32 +11,34 @@ interface TransactionListProps extends TransactionItemProps {
 interface AmountListProps {
   transactions: TransactionListProps[]
 }
-
 interface CategoryDictionary<TValue> {
   [key: string]: TValue
 }
 
-interface CategoryValues {
+interface Category {
   id: string,
-  amount: number,
+  deposit: number,
+  withdrawal: number,
   name: string,
   color: string,
 }
 
 export default function AmountList ({ transactions }: AmountListProps) {
-  const categories = useMemo(() => {
-    const list = {} as CategoryDictionary<CategoryValues>
+  const categoriesList = useMemo(() => {
+    const processor = {} as CategoryDictionary<CategoryValues>
+  
+    transactions.forEach((transaction: TransactionListProps) => {
+      const { amount, type } = transaction
 
-    transactions.forEach(({ id, amount, category: { name, color } }: TransactionListProps) => {
-      if (list[name] === undefined) {
-        list[name] = { id, amount, color, name }
-      } else {
-        list[name].amount += amount
+      if (processor[transaction.name] === undefined) {
+        processor[transaction.name] = new Processor(transaction)
       }
+
+      processor[transaction.name].put(amount, type)
     })
 
-    return list
-  }, [])
+    return Object.values(processor)
+  }, [transactions])
 
   const amountFormatter = useCallback((amount: number) => {
     return new Intl.NumberFormat('pt-BR',
@@ -46,12 +48,40 @@ export default function AmountList ({ transactions }: AmountListProps) {
 
   return (
     <Container>
-      {Object.values(categories || []).map(({ id, amount, name, color }: CategoryValues) => (
+      {categoriesList.map(({ id, amount, name, color }: CategoryList) => (
         <Item key={id} borderColor={color}>
           <Title>{name}</Title>
-          <Amount>{amountFormatter(amount)}</Amount>
+          <Amounts>
+            {amountFormatter(amount)}
+          </Amounts>
         </Item>
       ))}
     </Container>
   )
+}
+
+class Processor {
+  deposit: number;
+  withdrawal: number;
+
+  constructor(public transaction: TransactionListProps) {
+    this.deposit = 0
+    this.withdrawal = 0
+  }
+
+  put (amount: number, type: string) {
+    if (type === "deposit") {
+      this.deposit += amount
+    }
+
+    if (type === "withdrawal") {
+      this.withdrawal += amount
+    }
+  }
+
+  get () {
+    const { deposit, withdrawal, transaction: { id, category: { name, color } } } = this
+    return { id, deposit, withdrawal, name, color }
+  }
+
 }
--- apps/gofinances/src/components/AmountList/styles.ts
+++ apps/gofinances/src/components/AmountList/styles.ts
@@ -1,16 +1,21 @@
 import styled from 'styled-components/native';
+import { FontAwesome } from '@expo/vector-icons';
 
 interface ItemProps {
   borderColor: string,
 }
 
-export const Container = styled.View`
-  margin-top: 16px;
+interface TypeProps {
+  type: 'deposit' | 'withdrawal',
+}
+
+export const Container = styled.ScrollView`
+  padding: 16px 24px;
   width: 100%;
 `
 
 export const Item = styled.View<ItemProps>`
-  margin: 4px 24px;
+  margin: 4px 0px;
   border-radius: 6px;
   border-left-width: 6px;
   border-left-color: ${(props) => props.borderColor};
@@ -27,7 +32,34 @@ export const Title = styled.Text`
   font-size: 16px;
 `
 
-export const Amount = styled.Text`
+export const Amounts = styled.Text`
   font-family: ${({ theme }) => theme.fonts.bold};
   font-size: 16px;
+`
+
+export const Icon = styled(FontAwesome).attrs((props) => ({
+  ...props,
+  size: props.size || 40,
+}))<TypeProps>`
+
+  ${({ type }) => type === 'deposit' && css`
+    color: ${({ theme }) => theme.colors.success};
+  `}
+
+  ${({ type }) => type === 'withdrawal' && css`
+    color: ${({ theme }) => theme.colors.attention};
+  `}
+`
+
+export const Amount = styled.Text<TypeProps>`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: 16px;
+
+  ${({ type }) => type === 'deposit' && css`
+    color: ${({ theme }) => theme.colors.success};
+  `}
+
+  ${({ type }) => type === 'withdrawal' && css`
+    color: ${({ theme }) => theme.colors.attention};
+  `}
 `
