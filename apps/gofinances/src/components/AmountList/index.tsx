3e662df39b580687e92081005ed88a489167422b
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-05-13 18:44:36 -0300

    refactor: adjusts category in Amount

--- apps/gofinances/src/components/AmountList/index.tsx
+++ apps/gofinances/src/components/AmountList/index.tsx
@@ -1,31 +1,66 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
+
+import { TransactionListProps } from '../../screens/Dashboard';
+import CATEGORIES from '../../global/categories';
 
 import { Container, Item, Title, Amounts, Amount, Icon, Text } from './styles';
 
 interface AmountListProps {
-  categories: Category[]
+  transactions: TransactionListProps[]
+}
+
+interface Dictionary<TValue> {
+  [key: string]: TValue
 }
 
 interface Category {
+  id: number,
   deposit: number,
   withdrawal: number,
   name: string,
   color: string,
 }
 
-export default function AmountList ({ categories }: AmountListProps) {
+export default function AmountList ({ transactions }: AmountListProps) {
   const amountFormatter = useCallback((amount: number) => {
     return new Intl.NumberFormat('pt-BR',
       { style: 'currency', currency: 'BRL' }
     ).format(amount)
   }, [])
 
+  const [categories, totals] = useMemo(() => {
+    const processor = {} as Dictionary<Category>
+    let deposit = 0
+    let withdrawal = 0
+
+    transactions.forEach((transaction: TransactionListProps) => {
+      const { amount, type, category } = transaction
+      const { id, name, color } = CATEGORIES[category]
+
+      if (processor[name] === undefined) {
+        processor[name] = { deposit: 0, withdrawal: 0, id, name, color }
+      }
+
+      if (type === 'deposit') {
+        processor[name].deposit += amount
+        deposit += amount
+      }
+
+      if (type === 'withdrawal') {
+        processor[name].withdrawal += amount
+        withdrawal += amount
+      }
+    })
+
+    return [Object.values(processor), { deposit, withdrawal }]
+  }, [])
+
   return (
     <Container>
       {categories.map((category: Category) => {
-        const { deposit, withdrawal, name, color } = category
+        const { deposit, withdrawal, id, name, color } = category
         return (
-          <Item key={name} borderColor={color}>
+          <Item key={id} borderColor={color}>
             <Title>{name}</Title>
             <Amounts>
               <Amount>
