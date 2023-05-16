1fd9fc81ca2b9da9f60fa2b622f1cc26ac7c00f4
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-05-15 21:53:15 -0300

    refactor: restores proccessing on Summary

--- apps/gofinances/src/screens/Summary/index.tsx
+++ apps/gofinances/src/screens/Summary/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -6,17 +6,62 @@ import { $transactions } from '../../global/storage';
 import { TransactionItemProps } from '../../components/Transaction';
 import ActivityIndicator from '../../components/ActivityIndicator';
 import AmountList from '../../components/AmountList';
+import Chart from '../../components/Chart';
+import CATEGORIES from '../../global/categories';
 
-import { Container, Header, Title, AmountButtons, AmountButton, Icon } from './styles';
+import { Container, Header, Title } from './styles';
 
 export interface TransactionListProps extends TransactionItemProps {
   id: string,
 }
 
+interface Dictionary<TValue> {
+  [key: string]: TValue
+}
+
+interface Category {
+  id: number,
+  deposit: number,
+  withdrawal: number,
+  max: number,
+  name: string,
+  color: string,
+}
+
 export default function Summary () {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [transactions, setTransactions] = useState<TransactionListProps[]>([])
-  const [amountFilter, setAmountFilter] = useState<string>('deposit')
+
+  const categories = useMemo(() => {
+    const categories = {} as Dictionary<Category>
+
+    transactions.forEach((transaction: TransactionListProps) => {
+      const { amount, type, category } = transaction
+      const { id, name, color } = CATEGORIES[category]
+
+      if (categories[name] === undefined) {
+        categories[name] = { deposit: 0, withdrawal: 0, max: 0, id, name, color }
+      }
+
+      if (type === 'deposit') {
+        categories[name].deposit += amount
+      }
+
+      if (type === 'withdrawal') {
+        categories[name].withdrawal += amount
+      }
+
+      if (categories[name].deposit > categories[name].max) {
+        categories[name].max = categories[name].deposit
+      }
+
+      if (categories[name].withdrawal > categories[name].max) {
+        categories[name].max = categories[name].withdrawal
+      }
+    })
+
+    return Object.values(categories)
+  }, [transactions])
 
   const loadTransactions = async () => {
     setIsLoading(true)
@@ -40,18 +85,11 @@ export default function Summary () {
     <Container>
       <Header>
         <Title>Resumo</Title>
-
-        <AmountButtons>
-          <AmountButton onPress={() => setAmountFilter('deposit')}>
-            <Icon name="arrow-circle-up" type="deposit" />
-          </AmountButton>
-          <AmountButton onPress={() => setAmountFilter('withdrawal')}>
-            <Icon name="arrow-circle-down" type="withdrawal" />
-          </AmountButton>
-        </AmountButtons>
       </Header>
 
-      <AmountList transactions={transactions} amountFilter={amountFilter}  />
+      <Chart categories={categories} />
+
+      <AmountList categories={categories} />
     </Container>
   )
 }
--- apps/gofinances/src/screens/Summary/styles.ts
+++ apps/gofinances/src/screens/Summary/styles.ts
@@ -28,35 +28,7 @@ export const Header = styled.View`
 `
 
 export const Title = styled.Text`
-  padding-left: 46px;
   font-size: 18px;
   font-family: ${({ theme }) => theme.fonts.regular};
   color: ${({ theme }) => theme.colors.shape};
-`
-
-export const AmountButtons = styled.View`
-  flex-direction: row;
-  align-items: center;
-`
-
-export const AmountButton = styled(TouchableOpacity).attrs((props) => ({
-  ...props,
-  activeOpacity: props.activeOpacity || 0.6,
-}))`
-  opacity: 0.6;
-  margin-left: 8px;
-`
-
-export const Icon = styled(FontAwesome).attrs((props) => ({
-  ...props,
-  size: props.size || 20,
-}))<TypeProps>`
-
-  ${({ type }) => type === 'deposit' && css`
-    color: ${({ theme }) => theme.colors.success};
-  `}
-
-  ${({ type }) => type === 'withdrawal' && css`
-    color: ${({ theme }) => theme.colors.attention};
-  `}
 `
