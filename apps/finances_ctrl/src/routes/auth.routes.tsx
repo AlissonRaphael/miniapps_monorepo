38905e5844705934cf49d8b79d7fd322c372febf
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-06-05 21:22:26 -0300

    feat: creates auth-routes

---
+++ apps/finances_ctrl/src/routes/auth.routes.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '../screens/SignIn';
+
+const Stack = createStackNavigator();
+
+export default function AuthRoutes () {
+  return (
+    <Stack.Navigator screenOptions={{
+      headerShown: false,
+    }}>
+      <Stack.Screen name="SignIn" component={SignIn} />
+    </Stack.Navigator>
+  )
+}
