42e98f5994a52e80edd5b4e6e2e0ef6819e4fe49
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-06-11 23:58:47 -0300

    feat: implements dark theme

--- apps/finances_ctrl/src/global/theme.ts
+++ apps/finances_ctrl/src/global/theme.ts
@@ -25,3 +25,31 @@ export default {
     bold: 'Poppins_700Bold',
   }
 }
+
+export const dark = {
+  colors: {
+    primary: '#5636D3',
+
+    secondary: '#ff872c',
+    secondary_light: 'rgba(255,135,44,0.3)',
+
+    success: '#12A454',
+    success_light: 'rgba(18,164,84,0.5)',
+
+    attention: '#E83F5B',
+    attention_light: 'rgba(232,63,91,0.5)',
+
+    shape: '#fff',
+    title: '#363f5f',
+    text: '#969CB2',
+    text_invert: '#f0f0f0',
+    text_light: '#ffffff',
+    text_dark: '#000000',
+    background: '#F0F2F5'
+  },
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  }
+}
